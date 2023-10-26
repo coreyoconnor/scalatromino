67095d5477b7b5dabd3160a3eb15abb0227a845f
Corey O'Connor <coreyoconnor@gmail.com>
2023-10-26 09:25:11 -0700

    draw a red box

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,6 +3,7 @@ import glib.all.*
 import gtk.all.*
 import gtk.fluent.*
 import scala.scalanative.unsafe.*
+import libcairo.all.*
 
 @main def example =
   gtk_init()
@@ -14,9 +15,9 @@ import scala.scalanative.unsafe.*
 
       gtk_window_set_title(
         window.asInstanceOf[Ptr[GtkWindow]],
-        c"Hello from Scala Native"
+        c"Blocks"
       )
-      gtk_window_set_default_size(window.asPtr[GtkWindow], 200, 200)
+      gtk_window_set_default_size(window.asPtr[GtkWindow], 640, 480)
 
       val box = gtk_box_new(GtkOrientation.GTK_ORIENTATION_VERTICAL, 0)
       gtk_widget_set_halign(box, GtkAlign.GTK_ALIGN_CENTER)
@@ -34,6 +35,21 @@ import scala.scalanative.unsafe.*
       g_signal_connect(button, c"clicked", printHello)
 
       gtk_box_append(box.asPtr[GtkBox], button)
+
+      val drawingArea = gtk_drawing_area_new().asPtr[GtkDrawingArea]
+      val drawingFunction = CFuncPtr5.fromScalaFunction {
+        (drawingArea: Ptr[GtkDrawingArea], cr: Ptr[cairo_t], width: CInt, height: CInt, data: gpointer) => {
+            cairo_set_source_rgb(cr, 1, 0, 0) // Set color to red
+            cairo_rectangle(cr, 0, 0, width, height) // Draw a rectangle
+            cairo_fill(cr) // Fill the rectangle
+        }
+      }
+
+      gtk_drawing_area_set_content_width(drawingArea, 640)
+      gtk_drawing_area_set_content_height(drawingArea, 480)
+      gtk_drawing_area_set_draw_func(drawingArea, drawingFunction.asInstanceOf[GtkDrawingAreaDrawFunc], gpointer(null), GDestroyNotify(null))
+
+      gtk_box_append(box.asPtr[GtkBox], drawingArea.asPtr[GtkWidget])
 
       gtk_widget_show(window)
   }
