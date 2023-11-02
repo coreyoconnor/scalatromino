3aa2273292bf4dd3c42d494da6978d2de23a01ff
Corey O'Connor <coreyoconnor@gmail.com>
2023-11-01 22:17:00 -0700

    Nicer color scheme

--- src/main/scala/GameRenderer.scala
+++ src/main/scala/GameRenderer.scala
@@ -4,18 +4,52 @@ import gtk.fluent.*
 import libcairo.all.*
 import scala.scalanative.unsafe.*
 
-def grayPulse(micros: Long): (Double, Double, Double) = {
-  val min = 0.35
-  val max = 0.50
-  val period = 10.0
-
-  val t = micros.toDouble / 1000000.0
-  val v = 1.0 - ((scala.math.cos(2.0 * math.Pi * t / period) + 1.0)/2.0 * (max - min) + min)
-
-  (v, v, v)
-}
-
 object GameRenderer:
+  val minHeight = 768
+  val minWidth = minHeight * 9 / 18
+
+  type Color = (Double, Double, Double)
+
+  def pulse(micros: Long, low: Color, high: Color): Color = {
+    val period = 8.0
+
+    val t = micros.toDouble / 1000000.0
+    val v1 = ((scala.math.cos(2.0 * math.Pi * t / period) + 1.0)/2.0 * (high._1 - low._1) + low._1)
+    val v2 = ((scala.math.cos(2.0 * math.Pi * t / period) + 1.0)/2.0 * (high._2 - low._2) + low._2)
+    val v3 = ((scala.math.cos(2.0 * math.Pi * t / period) + 1.0)/2.0 * (high._3 - low._3) + low._3)
+
+    (v1, v2, v3)
+  }
+
+  case class ColorScheme(
+    background: Color,
+    backgroundGrid: Color,
+    activeOutline: Color,
+    activeFill: Color,
+    gridOutline: Color,
+    gridFill: Color
+  )
+
+  def colorScheme(micros: Long, phase: GamePhase): ColorScheme =
+    phase match {
+      case GamePhase.GameOver => ColorScheme(
+        background = pulse(micros, (0.8, 0.5, 0.5), (0.9, 0.55, 0.55)),
+        backgroundGrid = (0.0, 0.0, 0.0),
+        activeOutline = (0.2, 0.2, 0.2),
+        activeFill = (0.1, 0.1, 0.1),
+        gridOutline = (0.8, 0.8, 0.9),
+        gridFill = pulse(micros, (0.8, 0.8, 0.5), (0.9, 0.55, 0.55)),
+      )
+      case _ => ColorScheme(
+        background = pulse(micros, (0.25, 0.25, 0.25), (0.28, 0.28, 0.28)),
+        backgroundGrid = (0.3, 0.3, 0.3),
+        activeOutline = (0.9, 0.9, 0.9),
+        activeFill = pulse(micros, (0.1, 0.7, 0.2), (0.1, 0.75, 0.25)),
+        gridOutline = (0.0, 0.6562, 0.4843),
+        gridFill = pulse(micros, (0.65625, 0.0, 0.1679), (0.75625, 0.0, 0.2679)),
+      )
+    }
+
   def render(drawingArea: Ptr[GtkDrawingArea],
              cr: Ptr[cairo_t],
              width: CInt, height: CInt,
@@ -23,29 +57,28 @@ object GameRenderer:
              deltaT: Double,
              micros: Long): Unit = {
 
-    if (state.phase == GamePhase.GameOver) {
-      val color = (0, 1, 0)
-      cairo_set_source_rgb(cr, color._1, color._2, color._3)
-      cairo_rectangle(cr, 0, 0, width, height)
-      cairo_fill(cr)
-    } else {
-      val color = grayPulse(micros)
-      cairo_set_source_rgb(cr, color._1, color._2, color._3)
-      cairo_rectangle(cr, 0, 0, width, height)
-      cairo_fill(cr)
-    }
+    val cs = colorScheme(micros, state.phase)
 
-    val pieceSize = 20
+    cairo_set_source_rgb(cr, cs.background._1, cs.background._2, cs.background._3)
+    cairo_rectangle(cr, 0, 0, width, height)
+    cairo_fill(cr)
+
+    val pieceSize = width / state.grid.width
 
     for {
       y <- 0 until state.grid.height
       x <- 0 until state.grid.width
     } {
       if (state.grid(x, y).exists(_ != GridState.Empty)) {
-        cairo_set_source_rgb(cr, 0, 0, 0)
+        cairo_set_source_rgb(cr, cs.gridFill._1, cs.gridFill._2, cs.gridFill._3)
         cairo_rectangle(cr, x * pieceSize, y * pieceSize, pieceSize, pieceSize)
         cairo_fill(cr)
-        cairo_set_source_rgb(cr, 0.8, 0.8, 0.9)
+        cairo_set_source_rgb(cr, cs.gridOutline._1, cs.gridOutline._2, cs.gridOutline._3)
+        cairo_set_line_width(cr, 2.0)
+        cairo_rectangle(cr, x * pieceSize, y * pieceSize, pieceSize, pieceSize)
+        cairo_stroke(cr)
+      } else {
+        cairo_set_source_rgb(cr, cs.backgroundGrid._1, cs.backgroundGrid._2, cs.backgroundGrid._3)
         cairo_set_line_width(cr, 1.0)
         cairo_rectangle(cr, x * pieceSize, y * pieceSize, pieceSize, pieceSize)
         cairo_stroke(cr)
@@ -63,10 +96,10 @@ object GameRenderer:
           val outX = activePiece.posX + x - layout.centerX
           val outY = activePiece.posY + y - layout.centerY
 
-          cairo_set_source_rgb(cr, 1, 0, 0)
+          cairo_set_source_rgb(cr, cs.activeFill._1, cs.activeFill._2, cs.activeFill._3)
           cairo_rectangle(cr, outX * pieceSize, outY * pieceSize, pieceSize, pieceSize)
           cairo_fill(cr)
-          cairo_set_source_rgb(cr, 0, 0.2, 0.2)
+          cairo_set_source_rgb(cr, cs.activeOutline._1, cs.activeOutline._2, cs.activeOutline._3)
           cairo_set_line_width(cr, 2.0)
           cairo_rectangle(cr, outX * pieceSize, outY * pieceSize, pieceSize, pieceSize)
           cairo_stroke(cr)
--- src/main/scala/UILayout.scala
+++ src/main/scala/UILayout.scala
@@ -10,26 +10,27 @@ object UILayout:
             startGameButton: Ptr[GtkWidget],
             drawingArea: Ptr[GtkDrawingArea]
             ): Unit = {
+    gtk_drawing_area_set_content_width(drawingArea, GameRenderer.minWidth)
+    gtk_drawing_area_set_content_height(drawingArea, GameRenderer.minHeight)
 
     gtk_window_set_title(
       window.asInstanceOf[Ptr[GtkWindow]],
       c"Scalatromino"
     )
 
-    gtk_window_set_default_size(window.asPtr[GtkWindow], 640, 480)
+    val gameControls = gtk_box_new(GtkOrientation.GTK_ORIENTATION_HORIZONTAL, 0)
+    gtk_widget_set_halign(gameControls, GtkAlign.GTK_ALIGN_START)
+    gtk_widget_set_valign(gameControls, GtkAlign.GTK_ALIGN_START)
+    gtk_box_append(gameControls.asPtr[GtkBox], startGameButton)
 
-    val box = gtk_box_new(GtkOrientation.GTK_ORIENTATION_VERTICAL, 0)
-    gtk_widget_set_halign(box, GtkAlign.GTK_ALIGN_CENTER)
-    gtk_widget_set_valign(box, GtkAlign.GTK_ALIGN_CENTER)
+    val topLevel = gtk_box_new(GtkOrientation.GTK_ORIENTATION_VERTICAL, 0)
+    gtk_widget_set_halign(topLevel, GtkAlign.GTK_ALIGN_CENTER)
+    gtk_widget_set_valign(topLevel, GtkAlign.GTK_ALIGN_CENTER)
 
-    gtk_window_set_child(window.asPtr[GtkWindow], box)
+    gtk_box_append(topLevel.asPtr[GtkBox], gameControls.asPtr[GtkWidget])
+    gtk_box_append(topLevel.asPtr[GtkBox], drawingArea.asPtr[GtkWidget])
 
-    gtk_box_append(box.asPtr[GtkBox], startGameButton)
-
-    gtk_drawing_area_set_content_width(drawingArea, 1024)
-    gtk_drawing_area_set_content_height(drawingArea, 768)
-
-    gtk_box_append(box.asPtr[GtkBox], drawingArea.asPtr[GtkWidget])
+    gtk_window_set_child(window.asPtr[GtkWindow], topLevel)
   }
 
 end UILayout
